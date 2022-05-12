5c4a9852329a674649b4c4d164edf450c81e4529
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-12 05:34:30 -0700

    swap

---
+++ functionPack2.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+/**
+ * _swap - swaps first and second elemensts of the stack
+ *@stack: stack
+ *@line_number: line number
+ */
+void _swap(stack_t **stack, unsigned int line_number)
+{
+	int i = 0;
+	stack_t *aux = *stack;
+
+	if (aux = NULL || aux->prev == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	i = aux->prev->n;
+	aux->prev->n = aux->n;
+	aux->n = i;
+}
--- main.c
+++ main.c
@@ -53,6 +53,7 @@ void search(char *mainToken, stack_t **stack, unsigned int line_number)
 		{"pall", _pall},
 		{"pint", _pint},
 		{"pop", _pop},
+		{"swap", _swap},
 		{NULL, NULL}
 	};
 
