9dd63feb531b63d5cdf73c8a1bced436f60f9ca9
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-11 17:21:06 -0700

    header

--- monty.h
+++ monty.h
@@ -21,9 +21,9 @@ extern int value;
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+int n;
+struct stack_s *prev;
+struct stack_s *next;
 } stack_t;
 
 /**
@@ -36,18 +36,14 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void readFile(char* name);
+void readFile(char *name);
 void search(char *mainToken, stack_t **stack, unsigned int line_number);
 int is_number(char *data);
 
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
-
-
-
-
 #endif
