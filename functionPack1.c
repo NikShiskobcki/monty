f2f449ca279b15abb50c1af90240984cc66714af
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-11 17:49:25 -0700

    push pall pint pop

--- functionPack1.c
+++ functionPack1.c
@@ -69,3 +69,22 @@ void _pint(stack_t **stack, unsigned int line_number)
 	}
 	printf("%i\n", (*stack)->n);
 }
+
+/**
+ * _pop- removes first item
+ * @stack: stack
+ * @line_number: line number
+ * */
+void _pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new;
+
+	if (!stack)
+	{
+		fprintf(stderr, "L%i: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	new = (*stack)->next;
+	free(*stack);
+	*stack = new;
+}
