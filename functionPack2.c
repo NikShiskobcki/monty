57b484299603d80251cba40c3c757567004bbf21
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-05-12 05:46:25 -0700

    swap

--- functionPack2.c
+++ functionPack2.c
@@ -10,13 +10,13 @@ void _swap(stack_t **stack, unsigned int line_number)
 	int i = 0;
 	stack_t *aux = *stack;
 
-	if (aux == NULL || aux->prev == NULL)
+	if (aux == NULL || aux->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	i = aux->prev->n;
-	aux->prev->n = aux->n;
+	i = aux->next->n;
+	aux->next->n = aux->n;
 	aux->n = i;
 }
